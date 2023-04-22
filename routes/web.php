dafd368891ef228f273318be96c57dd74ebe3b03
Bebras1 <simas.jar@gmail.com>
2023-04-22 13:16:35 +0300

    Cleaning routes

--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/greeting', function () {
-    return 'greeting';
-});
 Route::get('/', [\App\Http\Controllers\MainController::class, 'index']) -> name('home');
 Route::post('/checklogin', [\App\Http\Controllers\MainController::class, 'checklogin']);
 Route::get('/successlogin', [\App\Http\Controllers\ArticleController::class, 'loginGuest']);
